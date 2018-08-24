0943f5a18ec51ae624bb5ae08a0ae046e6b5d0f5
neveisa <smpdmohanty@gmail.com>
2018-08-24 15:32:34 +0530

    auto commit

--- ideam/tools/dweet2amqp.py
+++ ideam/tools/dweet2amqp.py
@@ -1,11 +1,16 @@
 import requests
 import time
 
+
+lastVal = -1
 while 1:
     try:
       r=requests.get("https://dweet.io/get/latest/dweet/for/sampy")
       val=r.json()['with'][0]['content']['brightness']
       val=int(val)
+      if lastVal==val:
+          print("Stale value: %s" % val)
+          continue
       print (time.time(), val)
       import os
       os.system("python3 rabbit_pub.py %s" % val)
