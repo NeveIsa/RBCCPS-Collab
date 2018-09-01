75999252f6cda877a6b7deb1f6530c7a09ddc748
neveisa <smpdmohanty@gmail.com>
2018-09-01 19:51:23 +0530

    auto commit by sampad

--- pi_tools/begin.sh
+++ pi_tools/begin.sh
@@ -4,9 +4,14 @@ sleep 20
 cd RBCCPS-Collab/MBDriver/
 screen -L -dmS drv python3 MBMASTER.py
 
+
+cd ../tinc
+screen -L -dmS tinc ./tinc.start
+
 cd ../ideam
 screen -L -dmS m2i  sh start.sh
-
 screen -L -dmS i2m python3 ideam2modbus.py
-sleep 2
+
+sleep 5
 screen -ls
+
