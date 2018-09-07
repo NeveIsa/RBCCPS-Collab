02d3b1cae593cf2a7ad6ba8414a846afc4542f10
neveisa <smpdmohanty@gmail.com>
2018-09-07 17:49:08 +0530

    auto commit by sampad

--- ideam/ideam2modbus.py
+++ ideam/ideam2modbus.py
@@ -117,7 +117,7 @@ while True:
     
     if not len(msgs):
         counter=(counter+10)%100
-        msg={"actuation":{"timestamp":datetime.datetime.now().isoformat(),"messageID":1234321,"brightness": counter}}
+        msgs=[{"actuation":{"timestamp":datetime.datetime.now().isoformat(),"messageID":1234321,"brightness": counter}}]
 
     for msg in msgs:
         msgBody = m.unpack(msg)
