d7da6f6f2aa13812d6e3d4fbc7976341e4becfb2
Your Name <you@example.com>
2018-09-18 18:37:45 +0530

    auto commit by root

--- pi_tools/begin.sh
+++ pi_tools/begin.sh
@@ -3,15 +3,16 @@
 [ -z $1 ] && sleep 20
 
 cd RBCCPS-Collab/MBDriver/
-screen -L -dmS drv python3 -B MBMASTER.py
+screen -L -dmS drv python3 MBMASTER.py
 
 
 cd ../tinc
 screen -L -dmS tinc ./tinc.start
 
-cd ../ideam
-screen -L -dmS m2i  sh start.sh
-screen -L -dmS i2m python3 -B ideam2modbus.py
+cd ../ideam/amqp
+#screen -L -dmS m2i  sh start.sh
+#screen -L -dmS i2m python3 ideam2modbus.py
+sh start.sh
 
 sleep 5
 screen -ls
